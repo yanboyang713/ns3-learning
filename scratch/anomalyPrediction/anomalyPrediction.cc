ae7b0492371d99ce367254fd7a15ad1cd1eef33e
Boyang Yan <yanboyang713@gmail.com>
2023-02-18 01:45:07 -0500

    update

--- scratch/anomalyPrediction/anomalyPrediction.cc
+++ scratch/anomalyPrediction/anomalyPrediction.cc
@@ -1,4 +1,4 @@
-//CXX="clang++" CXXFLAGS_EXTRA="-lpqxx -lpq" ./ns3 configure
+//CXX="clang++" LDFLAGS="-lpqxx -lpq" ./ns3 configure
 //./ns3 run "anomalyPrediction --pcap=false --printRoutes=true --numOfUAVs=3 --time=5 --step=1 --EnableMonitor=true --hostname=test"
 #include <iostream>
 #include <fstream>
